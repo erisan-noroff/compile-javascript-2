fa377e8e7335d9b90e928f22a65f93aa085e6c3b
erisan-noroff <192410141+erisan-noroff@users.noreply.github.com>
2026-09-13 18:57:23 +0200

    Corrected classes.

--- public/assets/scripts/components/toast-notification.js
+++ public/assets/scripts/components/toast-notification.js
@@ -75,13 +75,13 @@ function displayToast(type, title, message) {
     toastText.append(toastTitle);
 
     const toastMessage = document.createElement('p');
-    toastMessage.classList.add('toast__message', 'text-secondary');
+    toastMessage.classList.add('toast__body');
     toastMessage.innerText = message;
     toastText.append(toastMessage);
 
     const toastCloseBtn = document.createElement('button');
     toastCloseBtn.id = 'close-toast';
-    toastCloseBtn.classList.add('text-secondary');
+    toastCloseBtn.classList.add('toast__close');
     toastContainer.append(toastCloseBtn);
     const toastCloseIcon = document.createElement('span');
     toastCloseIcon.className = 'material-icons';
